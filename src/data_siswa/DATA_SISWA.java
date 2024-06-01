37b4dca7a5665bbb62757fb3b26d73fcaab34d7a
muhammadazizridwan <akungithub008gmail.com>
2024-06-01 11:37:37 +0700

    first commit

---
+++ src/data_siswa/DATA_SISWA.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package data_siswa;
+
+/**
+ *
+ * @author M. Aziz Ridwan
+ */
+public class DATA_SISWA {
+
+    
+    public static void main(String[] args) {
+        new APK().setVisible(true);
+    }
+    
+}
